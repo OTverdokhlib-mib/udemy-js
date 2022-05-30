c6403abb5e4a49ff4a1699d502550fb22e6f59ab
OTverdokhlib <oleksandr.tverdokhlib@outlook.com>
2022-05-30 22:40:38 +0300

    init project

---
+++ task_01/task_01.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let userName = 'John',
+    userNumber = 25;
+
+    userNumber = 24;
+
+console.log(userNumber);
---
+++ task_02/task_02.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const storeName = 'UAShop';
+
+let storeDescription = {
+    budget: 10000,
+    employees: ['Den', 'Mik', 'Alan'],
+    products: {
+        'belt': 200,
+        'boots': 300
+    },
+    open: true
+};
