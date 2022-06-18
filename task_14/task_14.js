c2b7821a09e87ef31eab9d15a0746879ed0c84bb
OTverdokhlib <oleksandr.tverdokhlib@outlook.com>
2022-06-18 12:59:53 +0300

    added tasks 14

---
+++ task_14/task_14.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    let studentsGroups = [], restStudents = [];
+    const quantityGroups = Math.floor(arr.length / 3);
+    let q = 1;
+
+    arr.sort();
+
+    for (let i = 0; i < quantityGroups; i++) {
+        studentsGroups[i] = [];
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i + 1 >= q * 3) { q++; }
+
+        if (i < q * quantityGroups) {
+            studentsGroups[q - 1].push(arr[i]);
+        } else {
+            restStudents.push(arr[i]);
+        }
+    }
+
+    studentsGroups.push(`Оставшиеся студенты: ${restStudents.length === 0 ? '-' : restStudents.join(', ')}`);
+    return studentsGroups;
+}
+
+sortStudentsByGroups(students);
